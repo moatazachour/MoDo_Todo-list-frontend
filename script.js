aa89251bcfd3d35378e9d3ee2ab2142db9fb43df
moatazachour <moatazachour84@gmail.com>
2026-04-23 23:18:40 +0100

    Try hosting to vercel

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const ICONS = {
   info: `<svg viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"/><line x1="12" y1="8" x2="12" y2="12"/><line x1="12" y1="16" x2="12.01" y2="16"/></svg>`,
 };
 
-const API_BASE = "http://modo-todolist.runasp.net/api";
+const API_BASE = "/api";
 
 let currentUserObj;
 let allTasks = [];
